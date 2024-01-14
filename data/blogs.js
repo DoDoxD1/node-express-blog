2efeed38c8f68f4510735f381030b2dbc4d15ce6
arihant <aunujain53@gmail.com>
2024-01-14 23:48:43 +0530

    first commit

---
+++ data/blogs.js
@@ -0,0 +1,29 @@
+blogs = [
+  {
+    title: 'Titile 1',
+    content: 'Content 1',
+    slug: '1',
+  },
+  {
+    title: 'Titile 2',
+    content: 'Content 2',
+    slug: '2',
+  },
+  {
+    title: 'Titile 3',
+    content: 'Content 3',
+    slug: '3',
+  },
+  {
+    title: 'Titile 4',
+    content: 'Content 4',
+    slug: '4',
+  },
+  {
+    title: 'Titile 5',
+    content: 'Content 5',
+    slug: '5',
+  },
+]
+
+module.exports = blogs;
